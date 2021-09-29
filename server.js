8a86d1305b765c6a4a27ad57449786cedc77ba47
Maximilian Rice <maximilian.rice1@gmail.com>
2021-09-29 16:21:21 -0400

    completes new route

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+// ========== IMPORT DEPENDENCIES ==========
 const express = require('express');
+const morgan = require("morgan")
 const app = express();
 const port = 3000
 const methodOverride = require('method-override')
+const mongoose = require("mongoose")
 const Pokemon = require('./models/pokemon.js');
 
 // ========== MIDDLEWARE ==========
+
+
 
 // ========== ROUTES ==========
 // INDEX
@@ -19,6 +24,9 @@ app.get('/pokemon/new', (req, res) => {
 })
 
 // CREATE
+app.post("/pokemon", (req, res) => {
+
+})
 
 // EDIT
 
