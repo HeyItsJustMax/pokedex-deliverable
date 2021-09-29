0bd15686bec47b2c075b6a0a65527d6bc7b918a8
Maximilian Rice <maximilian.rice1@gmail.com>
2021-09-28 22:02:53 -0400

    first commit

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const app = express();
 const port = 3000
-// const Pokemon = require('../models/pokemon.js');
+const methodOverride = require('method-override')
+const Pokemon = require('./models/pokemon.js');
 
 // ========== MIDDLEWARE ==========
 
 // ========== ROUTES ==========
 // INDEX
-app.get('/', (req, res) => {
-res.render('index.ejs', { data: Pokemon });
+app.get('/pokemon/', (req, res) => {
+    res.render('index.ejs', { data: Pokemon});
 });
 
+// NEW
+app.get('/pokemon/new', (req, res) => {
+    res.render('new.ejs', {})
+
+})
+
+// CREATE
+
+// EDIT
+
+// DESTROY
+
+// UPDATE
 
 // SHOW
-app.get('/:id', (req, res) => {
+app.get('/pokemon/:id', (req, res) => {
 res.render('show.ejs', { data: Pokemon[req.params.id] });
 });
 
